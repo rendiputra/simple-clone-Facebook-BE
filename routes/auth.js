b46f9e6135a93cdcb56f41ccdb82dab35826d159
Rendi Putra Pradana <rendiputrapradana@gmail.com>
2021-10-17 02:05:07 +0700

    create login user

--- routes/auth.js
+++ routes/auth.js
@@ -25,6 +25,18 @@ router.post("/register", async (req, res) => {
   }
 });
 
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({email: req.body.email});
+    !user && res.status(404).json("user not found");
 
+    const validatePassword = await bcrypt.compare(req.body.password, user.password)
+    !validatePassword && res.status(400).json("wrong password");
+
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
