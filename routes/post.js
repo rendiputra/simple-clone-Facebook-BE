7be164d0ec8508d7bd767be6393bca93799e11d5
Rendi Putra Pradana <rendiputrapradana@gmail.com>
2021-10-23 02:19:26 +0700

    Feature: like or unliked a post

--- routes/post.js
+++ routes/post.js
@@ -42,5 +42,19 @@ router.delete("/:id", async(req, res) => {
   }
 });
 
-
+// like or unliked a post
+router.put("/:id/like", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if(!post.likes.includes(req.body.userId)) {
+      await post.updateOne({ $push: {likes: req.body.userId }});
+      res.status(200).json("The Post has been liked.");
+    } else {
+      await post.updateOne({ $pull: { likes: req.body.userId }});
+      res.status(200).json("The Post has been unliked.");
+    }
+  } catch(err) {
+    res.status(500).json(err);
+  }
+});
 module.exports = router;
