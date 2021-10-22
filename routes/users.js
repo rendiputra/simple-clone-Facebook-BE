b166aab8b5d206a0077b2b54d21879ead0b81d10
Rendi Putra Pradana <rendiputrapradana@gmail.com>
2021-10-22 16:19:41 +0700

    Feature: getting a user data

--- routes/users.js
+++ routes/users.js
@@ -35,9 +35,8 @@ router.put("/:id", async (req, res) => {
 // delete user
 router.delete("/:id", async (req, res) => {
   if (req.body.userId === req.params.id || req.body.isAdmin) {
-
     try {
-      const user = await User.findByIdAndDelete({ _id: req.params.id}, {
+      const user = await User.findByIdAndDelete(req.params.id, {
         $set: req.body,
       });
       res.status(200).json("Account has been deleted");
@@ -46,6 +45,17 @@ router.delete("/:id", async (req, res) => {
     }
   } else {
     return res.status(403).json("You can delete only your account!");
+  }
+});
+
+// getting a user
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, updatedAt, ...other } = user._doc;
+    res.status(200).json(other);
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
