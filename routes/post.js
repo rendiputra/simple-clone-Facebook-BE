2a6987f4ba0fe66c059344c717988f2a9d1e6ebe
Rendi Putra Pradana <rendiputrapradana@gmail.com>
2021-10-23 01:17:25 +0700

    Feature: Update a post

--- routes/post.js
+++ routes/post.js
@@ -12,4 +12,18 @@ router.post("/", async (req, res) => {
   }
 });
 
+// update a post
+router.put("/:id", async(req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if(post.userId === req.body.userId) {
+      await post.updateOne({ $set: req.body });
+      res.status(200).json("the post has been update");
+    } else {
+      res.status(403).json("You can update only your post!");
+    }
+  } catch(err) {
+    res.status(500).json(err);
+  }
+});
 module.exports = router;
