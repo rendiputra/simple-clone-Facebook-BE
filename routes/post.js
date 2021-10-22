44bf42106859bf20deb5c2f0d07a30524c545853
Rendi Putra Pradana <rendiputrapradana@gmail.com>
2021-10-23 01:47:54 +0700

    Feature: Delete a post

--- routes/post.js
+++ routes/post.js
@@ -26,4 +26,21 @@ router.put("/:id", async(req, res) => {
     res.status(500).json(err);
   }
 });
+
+// delete a post
+router.delete("/:id", async(req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if(post.userId === req.body.userId) {
+      await post.delete();
+      res.status(200).json("the post has been deleted");
+    } else {
+      res.status(403).json("You can delete only your post!");
+    }
+  } catch(err) {
+    res.status(500).json(err);
+  }
+});
+
+
 module.exports = router;
